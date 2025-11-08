98b338ea97f562d82f9cc6574ef44a1b8c1ea9b1
榊原昌彦 <sakakibara@rdlabo.jp>
2025-11-08 12:03:44 +0900

    fmt

--- demo/src/app/docs/docs-page.component.ts
+++ demo/src/app/docs/docs-page.component.ts
@@ -1,49 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  IonButton,
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonSegment,
-  IonSegmentButton,
-  IonItemGroup,
-  IonListHeader,
-  IonNote,
-  IonTitle,
-  IonToolbar,
-  Platform,
-} from '@ionic/angular/standalone';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar, Platform } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-docs-page',
   templateUrl: './docs-page.component.html',
   styleUrls: ['./docs-page.component.scss'],
-  imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    CommonModule,
-    FormsModule,
-    IonIcon,
-    IonButton,
-    IonButtons,
-    IonList,
-    IonItem,
-    IonSegment,
-    IonLabel,
-    IonSegmentButton,
-    IonListHeader,
-    IonLabel,
-    IonNote,
-    IonItemGroup,
-  ],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, IonButton, IonButtons],
 })
 export class DocsPage {
   readonly platform = inject(Platform);
