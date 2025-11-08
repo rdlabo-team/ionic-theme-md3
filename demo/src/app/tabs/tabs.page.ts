627284c99e2ad276d387ebd31198cac3754c4319
榊原昌彦 <sakakibara@rdlabo.jp>
2025-11-08 11:59:47 +0900

    feat(demo): add menu

--- demo/src/app/tabs/tabs.page.ts
+++ demo/src/app/tabs/tabs.page.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonMenu,
+  IonSplitPane,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonSplitPane, IonMenu, IonContent, IonList, IonItemGroup, IonItem],
 })
 export class TabsPage {}
